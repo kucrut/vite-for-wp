81e571f6ecd8469b920739f508b20cad1b82db8b
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-08-20 23:08:20 +0700

    Dev server plugin: Respect custom host & port in config

--- js/src/plugins/dev-server-config.ts
+++ js/src/plugins/dev-server-config.ts
@@ -2,29 +2,36 @@ import type { Plugin } from 'vite';
 
 import { choose_port } from '../utils/';
 
-const SERVER_HOST = 'localhost';
-const SERVER_PORT = 5173;
-
 export default function dev_server_config(): Plugin {
 	return {
 		apply: 'serve',
 		name: 'hm-dev-server',
 
 		async config( config ) {
-			const port = await choose_port( { host: SERVER_HOST, port: SERVER_PORT } );
-			const origin = `http://${ SERVER_HOST }:${ port }`;
+			const { server = {} } = config;
+			let { host = 'localhost', port = 5173, ...rest_server } = server;
+
+			// We need actual host name or IP address for choose_port() to work.
+			if ( typeof host === 'boolean' ) {
+				host = '0.0.0.0';
+			}
+
+			// Ensure chosen port is available because we need to enable strictPort below.
+			// If the chosen port is already in use, a free one will be selected.
+			port = await choose_port( { host, port } );
+			const origin = `http://${ host }:${ port }`;
 
 			return {
 				...config,
 				server: {
-					...( config.server || {} ),
+					...rest_server,
+					host,
 					origin,
 					port,
-					host: SERVER_HOST,
 					strictPort: true,
 					hmr: {
 						port,
-						host: SERVER_HOST,
+						host,
 						protocol: 'ws',
 					},
 				},
