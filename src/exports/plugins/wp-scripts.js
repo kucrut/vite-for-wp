8c94a65e30b5e02434ab79ea10563c1e601d5741
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-10-09 00:27:18 +0700

    Add plugin: wp-scripts

---
+++ src/exports/plugins/wp-scripts.js
@@ -0,0 +1,50 @@
+/** @typedef {{extraScripts?: Record<string, string>}} Options */
+
+import externals from 'vite-plugin-external';
+import globals from 'rollup-plugin-external-globals';
+import { wp_globals } from '../utils/wp-globals.js';
+
+/**
+ * WordPress scripts plugin
+ *
+ * This plugin provides easy access to built-in WordPress scripts and exclude them from the build.
+ *
+ * @type {(options: Options) => import('vite').Plugin}
+ * @return {import('vite').PluginOption[]} Vite plugins instances.
+ */
+export function wp_scripts( options = {} ) {
+	const { extraScripts = {} } = options;
+
+	const scripts = {
+		...wp_globals(),
+		...extraScripts,
+	};
+
+	/** @type { import('vite').Plugin } */
+	const plugin = {
+		name: 'v4wp:wp-scripts',
+
+		config() {
+			return {
+				build: {
+					rollupOptions: {
+						external: Object.keys( scripts ),
+						output: {
+							globals: scripts,
+						},
+					},
+				},
+			};
+		},
+	};
+
+	return [
+		plugin,
+		externals( {
+			externals: scripts,
+		} ),
+		globals( scripts, {
+			include: [ '**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx' ],
+		} ),
+	];
+}
