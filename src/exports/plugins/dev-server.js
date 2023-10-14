4860bba5215645c1ce621da018f63f4cc5b5c739
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-10-14 22:25:37 +0700

    Dev server plugin: Add configResolved & configureServer hooks

--- src/exports/plugins/dev-server.js
+++ src/exports/plugins/dev-server.js
@@ -1,3 +1,4 @@
+import fs from 'node:fs';
 import { mergeConfig } from 'vite';
 import { choose_port } from '../utils/choose-port.js';
 
@@ -8,6 +9,10 @@ import { choose_port } from '../utils/choose-port.js';
  * @return {import('vite').Plugin} Plugin object.
  */
 export function dev_server() {
+	const plugins_to_check = [ 'vite:react-refresh' ];
+	/** @type {string} */
+	let dev_manifest_file;
+
 	return {
 		apply: 'serve',
 		name: 'v4wp:dev-server',
@@ -44,6 +49,33 @@ export function dev_server() {
 						protocol: hmr_protocol,
 					},
 				},
+			} );
+		},
+
+		configResolved( config ) {
+			const { base, build, plugins, server } = config;
+			const prod_manifest_file = build.outDir + '/manifest.json';
+
+			// Remove build manifest as the PHP helper uses it to determine
+			// which manifest to load when enqueueing assets.
+			fs.rmSync( prod_manifest_file, { force: true } );
+
+			const data = {
+				base,
+				origin: server.origin,
+				port: server.port,
+				plugins: plugins_to_check.filter( i => plugins.some( ( { name } ) => name === i ) ),
+			};
+
+			dev_manifest_file = build.outDir + '/vite-dev-server.json';
+
+			fs.mkdirSync( build.outDir, { recursive: true } );
+			fs.writeFileSync( dev_manifest_file, JSON.stringify( data ), 'utf8' );
+		},
+
+		configureServer( server ) {
+			server.httpServer?.once( 'close', () => {
+				fs.rmSync( dev_manifest_file );
 			} );
 		},
 	};
