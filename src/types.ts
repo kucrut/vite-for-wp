f33b1463aa76a9e289526dbca192bc833c5be502
Dzikri Aziz <dzikri.aziz@gmail.com>
2025-03-14 22:43:46 +0700

    choose_port: Move type definition

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,19 @@ import { ResolvedConfig } from 'vite';
 
 export type Input = ResolvedConfig['build']['rollupOptions']['input'];
 
+export interface ChoosePortOptions {
+	/**
+	 * @default localhost
+	 * @description Host name or IP address, defaults to 'localhost'.
+	 */
+	host?: string
+	/**
+	 * @default 5173
+	 * @description Preferred port number, defaults to 5173.
+	 */
+	port?: number
+}
+
 export interface V4wpOptions {
 	/**
 	 * @description Entry points (optional, defaults to 'src/main.js'). See https://rollupjs.org/configuration-options/#input
